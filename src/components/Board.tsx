8d1adae6fc645056b8ce39d76cb4c50f1c47c0b3
ronbarrantes <ronbarrantes@gmail.com>
2023-06-18 19:24:19 -0700

    Cleanup

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -5,49 +5,11 @@ import classNames from 'classnames'
 import { useBoardState } from '@/state/boardState'
 import { buildCardData } from '@/utils/card'
 
-// interface DialogState {
-//   isOpen: boolean
-//   setIsOpen: () => void
-//   dialogContent: React.ReactNode
-//   dialogTitle: string
-//   handleDialog: ({
-//     content,
-//     title,
-//   }: {
-//     title: string
-//     content: React.ReactNode
-//   }) => void
-//   handleDialogClose: () => void
-// }
-
-// export const useDialogStore = create<DialogState>((set) => ({
-//   isOpen: false,
-//   setIsOpen: () => set((state) => ({ isOpen: !state.isOpen })),
-//   dialogTitle: '',
-//   dialogContent: null,
-//   handleDialog: ({ title, content }) =>
-//     set({ dialogTitle: title, dialogContent: content, isOpen: true }),
-//   handleDialogClose: () => set({ dialogContent: null, isOpen: false }),
-// }))
-
-// export const DialogV2 = () => {
-//   const dialogStore = useDialogStore()
-//   const { isOpen, dialogContent, dialogTitle, handleDialogClose } = dialogStore
-
-// What state do I need for my board and cards???
-
-// shuffled cards
-// selected card
-// matched cards
-// guesses
-// level
-// game over
-
 export interface CardData {
-  cardId: number
+  id: string
+  cardNumber: number
   matched: boolean
   selected: boolean
-  // handleClick: (cardId: number) => void
   cardImage: string
   cardText: string
   cardColor: string
@@ -57,9 +19,9 @@ interface CardProps extends CardData {
   handleMatch: (cardId: number) => void
 }
 
-const Card = ({ handleMatch, cardId, matched, selected }: CardProps) => {
+const Card = ({ handleMatch, cardNumber, matched, selected }: CardProps) => {
   const handleClick = () => {
-    handleMatch(cardId)
+    handleMatch(cardNumber)
   }
   return (
     <div
@@ -95,7 +57,7 @@ export const Board = () => {
   return (
     <main className="flex grow h-full flex-wrap overflow-hidden">
       {Object.values(cards).map((card, idx) => {
-        const id = `${card.cardId}__${idx}`
+        const id = card.id
         return <Card key={id} {...card} handleMatch={handleMatch} />
       })}
     </main>
