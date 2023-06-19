d92f182012d9791aaf0e7d8adb096ac0b7f42d5e
ronbarrantes <ronbarrantes@gmail.com>
2023-06-18 18:07:55 -0700

    Add card structure

---
+++ src/utils/card.ts
@@ -0,0 +1,36 @@
+const getCardImage = async (width = 200, height?: number) => {
+  !height ? (height = width) : height
+  const image = await fetch(`https://picsum.photos/${width}/${height}`)
+  const result = image.url
+  return result
+}
+
+const getImageList = async (count = 4) => {
+  const images = await Promise.all(
+    Array.from({ length: count }, () => getCardImage()),
+  )
+  return images
+}
+
+const makeColorRandomColor = () => {
+  const randomColor = Math.floor(Math.random() * 16777215).toString(16)
+  return `#${randomColor}`
+}
+
+const randomizeCardData = <T>(cardData: T[]) => ({
+  ...cardData.sort(() => Math.random() - 0.5),
+})
+
+export const buildCardData = async () => {
+  const images = await getImageList()
+  const cardData = images.map((image, idx) => ({
+    cardId: idx,
+    matched: false,
+    selected: false,
+    cardImage: image,
+    cardText: `Card ${idx}`,
+    cardColor: makeColorRandomColor(),
+  }))
+
+  return randomizeCardData([...cardData, ...cardData])
+}
