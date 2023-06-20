819aaa0c7868a8664ee46f17a3c0703c53862a79
ronbarrantes <ronbarrantes@gmail.com>
2023-06-19 17:12:11 -0700

    Add some extra elements to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,17 @@ import { Board } from '@/components/Board'
 import { useBoardState } from '@/state/boardState'
 
 const App = () => {
-  const { guessCount } = useBoardState()
+  const { guessCount, difficulty } = useBoardState()
 
   return (
     <div className="flex h-screen w-screen flex-col p-2">
-      <header>
-        <div>Matching game</div>
+      <header className="m-auto flex items-center justify-between lg:w-10/12">
+        <h1 className="text-2xl">Matching game</h1>
+        <span>Level: {difficulty}</span>
       </header>
       <Board />
-      <footer className="justify-center flex">
-        <div>Guess count: {Math.floor(guessCount)}</div>
+      <footer className="flex justify-center">
+        <div className="text-2xl">Guess count: {Math.floor(guessCount)}</div>
       </footer>
     </div>
   )
