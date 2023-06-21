54871b7012dd3da08702ab05b7d53e0b020a8d94
ronbarrantes <ronbarrantes@gmail.com>
2023-06-20 22:23:04 -0700

    Move blurry background

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -55,8 +55,8 @@ export const Board = () => {
   return (
     <main className="m-auto flex h-full grow flex-wrap overflow-hidden border border-red-500 2xl:w-10/12">
       {isGameOver && (
-        <div className="absolute left-0 top-0 flex h-full w-full items-center justify-center bg-black bg-opacity-50">
-          <div className="z-20 flex flex-col gap-3 rounded-lg bg-white p-4 text-black shadow-lg shadow-black">
+        <div className="absolute left-0 top-0 z-20 flex h-full w-full items-center justify-center bg-black bg-opacity-50 backdrop-blur-sm">
+          <div className="flex flex-col gap-3 rounded-lg bg-white p-4 text-black shadow-lg  shadow-black">
             <h2 className="text-center text-6xl">Game Over</h2>
             <button
               className="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
